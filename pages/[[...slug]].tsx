2e892532f79826f626ee16cbbf82a9be7f3ba02d
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2020-12-17 18:27:02 -0300

    change

--- pages/[[...slug]].tsx
+++
@@ -1,9 +0,0 @@
-import { Pages } from '../components/Pages/Pages'
-import { PagesStaticPaths } from '../components/Pages/PagesStaticPaths'
-import { PagesStaticProps } from '../components/Pages/PagesStaticProps'
-
-export default Pages
-
-export const getStaticPaths = PagesStaticPaths
-
-export const getStaticProps = PagesStaticProps
