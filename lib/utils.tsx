e4aaf58b18ab2ecc63aeb31b0c46793fd924ffbb
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2020-12-17 18:15:07 -0300

    changes

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -11,10 +11,6 @@ import "katex/dist/contrib/mhchem.js"
 import markdown from 'remark-parse'
 import gfm from 'remark-gfm'
 
-const branch = process.env.VERCEL_GIT_COMMIT_REF
-    ? process.env.VERCEL_GIT_COMMIT_REF : process.env.NODE_ENV == "development" 
-    ? "dev": "master"
-
 export const urlEnvDic = {
     [Env.development]: 'http://localhost:3000',
     [Env.preview]: 'https://questions.giordanelli.vercel.app',
@@ -68,16 +64,6 @@ export const questionParsed2MD = (isAmp: boolean, questionParsed: Question2) => 
     } as QuestionParsed
 }
 
-export async function getFileContentFromGHRepo(ghRepo: GitHub, filePathFromRoot: string){
-    const response = await fetch(`https://raw.githubusercontent.com/${ghRepo.username}/${ghRepo.repo}/${branch}/${filePathFromRoot}`);
-    const body =  await response.text();
-
-    if(response.status != 200)
-        throw new Error(body + " " + `https://raw.githubusercontent.com/${ghRepo.username}/${ghRepo.repo}/${branch}/${filePathFromRoot}`)
-
-    return body
-}
-
 export const absolute = (base, relative) => {
     let stack = base.split("/");
     let parts = relative.split("/");
@@ -94,80 +80,6 @@ export const absolute = (base, relative) => {
     }
     return stack.join("/");
 };
-
-export const yml2Nav = (text: string) => yaml.safeLoad(text) as Nav
-
-export const yml2NotebookList = (text: string) => yaml.safeLoad(text) as string[]
-
-export const getNavFromNotebook = async (ghRepo: GitHub, notebook: string) => {
-
-    try {
-        const nav = yml2Nav(await getFileContentFromGHRepo(ghRepo, `${notebook}/nav.yaml`))
-        
-        nav.questions = nav.questions.sort((a, b) => a.title > b.title ? 1: -1)
-
-        nav.menu = nav.menu.map(x => ({
-            ...x,
-            title: x.title,
-            topics: x.topics.map(y => {
-                const url = nav.questions.find(z => z.topic == y.topic)?.url
-                if(url){
-                    return {
-                        ...y,
-                        url: `${notebook}/${url}`
-                    }
-                }
-            }).filter(y => !!y)
-        }))
-
-        nav.questions = nav.questions.map(x => ({
-            ...x,
-            title: x.title.substring(0, 50),
-            file: `${notebook}/${x.file}`,
-            absolutUrl: `https://raw.githubusercontent.com/${ghRepo.username}/${ghRepo.repo}/${branch}/${notebook}/${x.file}`
-        }))
-
-        return nav
-    }
-    catch (e) {
-        throw new Error("Notebook Nav.yaml not found. "+ e)
-    }
-}
-
-export const getNavFromGHRepo = async (ghRepo: GitHub) => yml2NotebookList(await getFileContentFromGHRepo(ghRepo, `nav.yaml`))
-
-export async function getPathsFromNotebook(ghRepo: GitHub, notebook: string) {
-
-    const nav = await getNavFromNotebook(ghRepo, notebook)
-
-    const pathList: Path[] = nav.questions
-        .map(x => ({ 
-            params: { 
-                slug: [notebook].concat(x.url) // [enem,questao1]
-            } 
-        }))
-
-    pathList.unshift({
-        params: {
-            slug: [notebook]
-        }
-    })
-
-    return pathList
-}
-
-export async function getPathsFromGHRepo(ghRepo: GitHub) {
-
-    const notebookList = await getNavFromGHRepo(ghRepo)
-
-    let navListFromGhRepo: Path[] = []
-
-    for(let notebook of notebookList){
-        navListFromGhRepo = navListFromGhRepo.concat(await getPathsFromNotebook(ghRepo, notebook))
-    }
-
-    return navListFromGhRepo
-}
 
 export const questionsofConverter: FirebaseFirestore.FirestoreDataConverter<QuestionsOf> = {
     toFirestore(questionsof: QuestionsOf): FirebaseFirestore.DocumentData {
