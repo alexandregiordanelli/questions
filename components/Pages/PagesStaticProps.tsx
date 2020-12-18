68daffecb3689d001d4cba83d96f979841b9ad17
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2020-12-18 13:13:18 -0300

    changes

--- components/Pages/PagesStaticProps.tsx
+++ components/Pages/PagesStaticProps.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from 'next';
 import { PagesProps } from './Pages';
 import { Menu, Question2, Question2Basic, QuestionsOfDic } from '../../lib/types';
+import { urlEnv } from '../../lib/utils';
 
 export const PagesStaticProps: GetStaticProps<{} | PagesProps> = async (context) => {
     if (!context.params.slug)
@@ -12,11 +13,11 @@ export const PagesStaticProps: GetStaticProps<{} | PagesProps> = async (context)
     try {
         const questionsof = context.params.slug[0]
         
-        const questionsOfDicRes = await fetch(`/api/questionsof/${questionsof}`)
+        const questionsOfDicRes = await fetch(`${urlEnv}/api/questionsof/${questionsof}`)
         const questionsOfDic: QuestionsOfDic = await questionsOfDicRes.json()
         const questionsofData = questionsOfDic.data
 
-        const menuRes = await fetch(`/api/menu/${questionsof}`)
+        const menuRes = await fetch(`${urlEnv}/api/menu/${questionsof}`)
         const menu: Menu[] = await menuRes.json()
 
         questionsofData.menu = menu
@@ -24,10 +25,10 @@ export const PagesStaticProps: GetStaticProps<{} | PagesProps> = async (context)
         if (context.params.slug.length > 1) {
             const questionUrl = context.params.slug[1]
 
-            const questionRes = await fetch(`/api/question/${questionsof}/${questionUrl}`)
+            const questionRes = await fetch(`${urlEnv}/api/question/${questionsof}/${questionUrl}`)
             const question: Question2 = await questionRes.json()
 
-            const suggestionsRes = await fetch(`/api/suggestions/${questionsof}/${question.topic}`)
+            const suggestionsRes = await fetch(`${urlEnv}/api/suggestions/${questionsof}/${question.topic}`)
             const suggestions: Question2Basic[] = await suggestionsRes.json()
 
             return {
