ca2ffaf6bfb6cacd0afaa9a935f4caeed1b6521c
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2020-12-18 13:48:38 -0300

    next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,6 @@ module.exports = withPWA({
     },
     pwa: {
         dest: 'public',
-        disable: true//process.env.NODE_ENV === 'development',
+        disable: process.env.NODE_ENV === 'development',
     }
 })
