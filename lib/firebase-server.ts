177aed44a9b71c0df1c6f95e895d956ad47ab6b1
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2020-12-29 18:08:28 -0300

    firebase admin private key

--- lib/firebase-server.ts
+++ lib/firebase-server.ts
@@ -3,7 +3,7 @@ import admin, { ServiceAccount, credential } from "firebase-admin"
 if(!admin.apps.length){
     const serviceAccount: ServiceAccount = {
         projectId: "questionsofcom",
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         clientEmail: "firebase-adminsdk-o982d@questionsofcom.iam.gserviceaccount.com",
     }
 
