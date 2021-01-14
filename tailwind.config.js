1c2b69f3ccb114406076d3ddd9b5098c2cbb48f0
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-01-14 17:23:45 -0300

    teste

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
     purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
     darkMode: false, // or 'media' or 'class'
@@ -7,5 +9,15 @@ module.exports = {
     variants: {
         extend: {},
     },
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [
+        plugin(function({ addUtilities }) {
+            const newUtilities = {
+              '.max-h-1000px': {
+                maxHeight: 1000,
+              },
+            }
+      
+            addUtilities(newUtilities, ['responsive', 'hover'])
+        }),
+        require('@tailwindcss/forms')],
 }
