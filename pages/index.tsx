b764a089a1f59506dc0ada78e85d9c3b6a386d4c
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-01-18 16:42:25 -0300

    teste

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import getNotebooks from '../services/getNotebooks';
 import { Notebook } from '@prisma/client';
 import Link from 'next/link';
 import { Header } from '../components/Header';
+import { ChevronRightIcon, ArrowRightIcon } from '@primer/octicons-react';
 
 type PageProps = {
     notebooks: Notebook[]
@@ -15,6 +16,8 @@ type PageProps = {
 
 const Index: React.FC<PageProps> = props => {
     
+    const isAmp = useAmp()
+
     const notebooks = [
         {
             img: 0,
@@ -62,19 +65,52 @@ const Index: React.FC<PageProps> = props => {
         <div>
             <HeadHtml />
             <Header/> 
-            <div className="p-10 bg-gray-100" style={{
-backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 100 100'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%23d9d9d9' fill-opacity='0.4'%3E%3Cpath opacity='.5' d='M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9h4v1h-4v9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z'/%3E%3Cpath d='M6 5V0H5v5H0v1h5v94h1V6h94V5H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");`}}>
+            <div className="relative bg-gradient-to-b from-gray-800 to-gray-600">
+                {!isAmp  && <img className="opacity-30" src="main.jpg"/>}
+                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 container">
+                    <div className="
+                        text-center 
+                        text-4xl 
+                        sm:text-5xl
+                        md:text-6xl
+                        lg:text-7xl
+                        xl:text-8xl
+                        2xl:text-9xl
+                        leading-none 
+                        tracking-tight 
+                        font-extrabold 
+
+                        text-white 
+                        ">
+                        Prepare-se para a prova fazendo questões
+                    </div>
+                    {/* <div className="text-xl sm:text-4xl lg:text-5xl leading-none font-bold tracking-tight text-white">
+                        <ul>
+                            <li>Escolha a prova.</li>
+                            <li>Faça questões.</li>
+                            <li>Aprenda com a solução da questão.</li>
+                            <li>Analise suas estatísticas.</li>
+                        </ul>
+                    </div> */}
+                </div>
+                
+            </div>
+            
+            <div className="p-10 bg-gray-200" style={{backgroundImage: `url("graph-paper.svg")`}}>
                 <span className="font-semibold text-gray-400">Notebooks</span>
                 <div className="flex overflow-x-auto">
                 {notebooks.map((x, i) => {
                     //const imgSrc = fs.readFile(`${i}.jpg`)
-                    return <div className="bg-white shadow-md m-4 rounded-md p-5 flex-none">
-                        <img src={`/${x.img}.jpg`} height={200} width={200} />
-                        <h1 className="block ">{x.name}</h1>
-                        <span className="block">R${x.price}</span>
+                    return <div className="bg-white shadow-md m-4 rounded-md flex-none">
+                        {isAmp ? <amp-img src={`/${x.img}.jpg`} height={200} width={200} className="m-5" /> : <img src={`/${x.img}.jpg`} height={200} width={200} className="m-5" />}
 
-                        <Link href={""}><a className={"bg-gray-500 text-white rounded-lg float-right shadow-md px-4 py-2 font-semibold"}>Click</a></Link>
-                        {/* {x.tag} */}
+                        <div className="bg-gray-50 p-5 rounded-b-md">
+                            <h1 className="block font-medium">{x.name}</h1>
+                            <span className="block">R${x.price}</span>
+                            {/* <div className="flex justify-end mt-5 rounded-b-lg">
+                                <Link href={""}><a className={"bg-gray-500 text-white rounded-lg shadow-md px-4 py-2 font-semibold"}>Click</a></Link>
+                            </div> */}
+                        </div>
                     </div>
                 })}
                 </div>
