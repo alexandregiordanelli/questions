a99fdb0cbd71e20ed056ab39bcdee841bcab4c28
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-01-26 00:10:15 -0300

    amp

---
+++ pages/amp/[...tags].tsx
@@ -0,0 +1,5 @@
+import Pages from 'pages/[...tags]'
+
+export { getStaticProps, getStaticPaths } from 'pages/[...tags]'
+export const config = { amp: true }
+export default Pages
--- pages/amp/username/read/[username].tsx
+++
@@ -1,5 +0,0 @@
-import ReadUsernamePage from 'pages/username/read/[username]'
-
-export { getStaticProps, getStaticPaths } from 'pages/username/read/[username]'
-export const config = { amp: true }
-export default ReadUsernamePage
