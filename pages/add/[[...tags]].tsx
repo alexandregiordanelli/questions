9f7bc4505b2602ed02ab6b8261c8198d12e8682e
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-01-28 00:58:07 -0300

    add without SSG

--- pages/add/[[...tags]].tsx
+++ pages/add/[[...tags]].tsx
@@ -1,6 +1,4 @@
-import { NextPage, GetStaticProps, GetStaticPaths } from 'next'
-import { getCustomerByTag } from 'services/getCustomer'
-import { getNotebookByTags } from 'services/getNotebook'
+import { NextPage } from 'next'
 import { NotebookWithTopicsAndSubTopics } from 'lib/types'
 import { Customer } from '@prisma/client'
 import { Header } from 'components/Header'
@@ -8,16 +6,16 @@ import { EditCustomer } from 'components/EditCustomer'
 import { EditNotebook } from 'components/EditNotebook'
 import { EditQuestion } from 'components/EditQuestion'
 import { useData } from 'services/client/get'
+import { useRouter } from 'next/router'
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type CustomerPageProps = {}
 type NotebookPageProps = {
-  customer: Customer
+  customerTag: string
 } & CustomerPageProps
 type QuestionPageProps = {
-  notebook: NotebookWithTopicsAndSubTopics
+  notebookTag: string
 } & NotebookPageProps
-
 type PageProps = CustomerPageProps | NotebookPageProps | QuestionPageProps
 
 const CustomerPage: React.FC<CustomerPageProps> = () => {
@@ -30,7 +28,7 @@ const CustomerPage: React.FC<CustomerPageProps> = () => {
 }
 
 const NotebookPage: React.FC<NotebookPageProps> = (props) => {
-  const { data: customer } = useData<Customer>(`/api/${props.customer.username}`, props.customer)
+  const { data: customer } = useData<Customer>(`/api/${props.customerTag}`)
   return (
     <>
       <Header />
@@ -40,10 +38,9 @@ const NotebookPage: React.FC<NotebookPageProps> = (props) => {
 }
 
 const QuestionPage: React.FC<QuestionPageProps> = (props) => {
-  const { data: customer } = useData<Customer>(`/api/${props.customer.username}`, props.customer)
+  const { data: customer } = useData<Customer>(`/api/${props.customerTag}`)
   const { data: notebook } = useData<NotebookWithTopicsAndSubTopics>(
-    `/api/${props.customer.username}/${props.notebook.tag}`,
-    props.notebook
+    `/api/${props.customerTag}/${props.notebookTag}`
   )
   return (
     <>
@@ -53,60 +50,18 @@ const QuestionPage: React.FC<QuestionPageProps> = (props) => {
   )
 }
 
-export const Page: NextPage<PageProps> = (props) => {
-  if ('notebook' in props) {
-    return <QuestionPage customer={props.customer} notebook={props.notebook} />
-  } else if ('customer' in props) {
-    return <NotebookPage customer={props.customer} />
-  } else {
-    return <CustomerPage />
-  }
-}
-
-export const getStaticProps: GetStaticProps<PageProps> = async (context) => {
-  const tags = (context.params.tags as string[]) ?? []
+export const Page: NextPage<PageProps> = () => {
+  const route = useRouter()
+  const tags = (route.query.tags as string[]) ?? []
 
   const [customerTag, notebookTag] = tags
 
-  if (tags.length == 0) {
-    return {
-      props: {},
-    }
-  } else if (tags.length == 1) {
-    const customer = await getCustomerByTag(customerTag)
-
-    if (customer) {
-      return {
-        props: {
-          customer,
-        },
-        revalidate: 1,
-      }
-    }
-  } else if (tags.length == 2) {
-    const customer = await getCustomerByTag(customerTag)
-    const notebook = await getNotebookByTags(customerTag, notebookTag)
-
-    if (customer && notebook) {
-      return {
-        props: {
-          customer,
-          notebook,
-        },
-        revalidate: 1,
-      }
-    }
-  }
-
-  return {
-    notFound: true,
-  }
-}
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: 'blocking',
+  if (notebookTag) {
+    return <QuestionPage customerTag={customerTag} notebookTag={notebookTag} />
+  } else if (customerTag) {
+    return <NotebookPage customerTag={customerTag} />
+  } else {
+    return <CustomerPage />
   }
 }
 export default Page
