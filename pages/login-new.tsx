a0e123edd617a2f9e9b67c38fd6e6ef2634105e2
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-02-23 01:01:35 -0300

    login

--- pages/login-new.tsx
+++ pages/login-new.tsx
@@ -1,15 +1,28 @@
 import { NextPage } from 'next'
 import firebase from 'lib/firebase-client'
 import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 const Page: NextPage = () => {
+  const router = useRouter()
+  const signInSuccessUrl = (router.query?.url as string) ?? '/'
+
   useEffect(() => {
     import('firebaseui').then((firebaseui) => {
       const config = {
-        signInSuccessUrl: '<url-to-redirect-to-on-success>',
+        signInSuccessUrl,
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          {
+            // Google provider must be enabled in Firebase Console to support one-tap
+            // sign-up.
+            provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            // Required to enable ID token credentials for this provider.
+            // This can be obtained from the Credentials page of the Google APIs
+            // console. Use the same OAuth client ID used for the Google provider
+            // configured with GCIP or Firebase Auth.
+            clientId: '872190920136-g962c5dhq2qctc3cj1hq8sdo3atqlpq5.apps.googleusercontent.com',
+          },
           firebase.auth.FacebookAuthProvider.PROVIDER_ID,
           firebase.auth.TwitterAuthProvider.PROVIDER_ID,
           // firebase.auth.GithubAuthProvider.PROVIDER_ID,
@@ -17,6 +30,7 @@ const Page: NextPage = () => {
           // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
           // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
         ],
+        credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
         // tosUrl and privacyPolicyUrl accept either url string or a callback
         // function.
         // Terms of service url/callback.
