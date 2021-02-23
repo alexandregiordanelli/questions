a4bd17afbd0e6aaaaa63e852c56575bec90511a7
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-02-23 01:08:53 -0300

    header

--- components/TemplateAdmin.tsx
+++ components/TemplateAdmin.tsx
@@ -1,14 +1,18 @@
 import { MenuAdmin } from 'components/MenuAdmin'
+import { Header } from './Header'
 
 export const TemplateAdmin: React.FC<{
   className?: string
 }> = (props) => {
   return (
-    <div className="h-screen flex">
-      <div className="w-60 bg-gray-800">
-        <MenuAdmin />
+    <div className="h-screen flex flex-col">
+      <Header />
+      <div className="flex-grow flex">
+        <div className="w-60 bg-gray-800">
+          <MenuAdmin />
+        </div>
+        <div className={`flex-1 overflow-auto ${props.className ?? ''}`}>{props.children}</div>
       </div>
-      <div className={`flex-1 overflow-auto ${props.className ?? ''}`}>{props.children}</div>
     </div>
   )
 }
