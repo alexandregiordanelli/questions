20a73cb0bcda0937248e64ecfba410947d010658
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-02-25 00:08:40 -0300

    changes

--- services/server/postCustomer.ts
+++ services/server/postCustomer.ts
@@ -2,20 +2,11 @@ import { prisma } from '../../prisma/prisma'
 import { Customer } from '@prisma/client'
 
 const postCustomer = async (_customer: Customer): Promise<Customer> => {
-  const customer = await prisma.customer.upsert({
+  let customer = await prisma.customer.upsert({
     create: {
       tag: _customer.tag,
       userId: _customer.userId,
       name: _customer.name,
-      media: {
-        create: {
-          ext: 'jpg',
-          mime: 'image/jpeg',
-          name: 'profile.jpg',
-          size: 0,
-          tag: 'profile.jpg',
-        },
-      },
     },
     update: {
       tag: _customer.tag,
@@ -26,6 +17,28 @@ const postCustomer = async (_customer: Customer): Promise<Customer> => {
       id: _customer.id ?? 0,
     },
   })
+
+  if (_customer.id != customer.id) {
+    const media = await prisma.media.create({
+      data: {
+        customerId: customer.id,
+        ext: 'jpg',
+        mime: 'image/jpeg',
+        name: 'profile.jpg',
+        size: 0,
+        tag: 'profile.jpg',
+      },
+    })
+
+    customer = await prisma.customer.update({
+      data: {
+        mediaId: media.id,
+      },
+      where: {
+        id: customer.id,
+      },
+    })
+  }
 
   return customer
 }
