9e0e47eae3d1cf40aa01322a5d47e8ec8697d979
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-02-26 18:00:56 -0300

    changes

--- components/Pages/Notebook/RightMenu.tsx
+++ components/Pages/Notebook/RightMenu.tsx
@@ -21,7 +21,7 @@ const RightMenu: React.FC<{
     <>
       <div className="overflow-auto static top-24 bg-gray-100 p-4 border rounded mb-12 lg:ml-12 lg:max-w-xs w-full lg:border-0 lg:bg-transparent lg:p-0">
         <label className="flex align-middle justify-between cursor-pointer" htmlFor="rightMenu">
-          <span>+ Questões</span>
+          <span className="mb-1 font-medium">+ Questões</span>
           <span className="lg:hidden">
             <ChevronDownIcon verticalAlign="middle" />
           </span>
@@ -33,24 +33,17 @@ const RightMenu: React.FC<{
           onChange={(x) => setToggleMenu(x.target.checked)}
           checked={toggleMenu}
         />
-        <ul className="hidden lg:block">
+        <ul className="hidden lg:block text-sm text-gray-700">
           {props.suggestions?.map((x, i) => (
             <li key={i}>
               {/* activeClassName="ativo" */}
               <Link href={`/${props.customerTag}/${props.notebookTag}/${x.tag}`}>
-                <a>{x.name}</a>
+                <a className="py-1 inline-block hover:underline">{x.name}</a>
               </Link>
             </li>
           ))}
         </ul>
       </div>
-
-      <style jsx>{`
-        {/* div {
-          max-height: calc(100vh - 110px - 24px);
-          grid-area: table-of-contents / table-of-contents / table-of-contents / table-of-contents;
-        } */}
-      `}</style>
     </>
   )
 }
