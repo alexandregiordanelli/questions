0d0aa0d8b9a22598d6a8bf8ac0888117f02ce109
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-03-13 15:38:18 -0300

    css

--- components/TemplateAdmin.tsx
+++ components/TemplateAdmin.tsx
@@ -1,11 +1,19 @@
 import { MenuAdmin } from 'components/MenuAdmin'
+import React from 'react'
 import { Header } from './Header'
+import { HeadHtml } from './HeadHtml'
 
 export const TemplateAdmin: React.FC<{
   className?: string
 }> = (props) => {
   return (
     <div className="h-screen flex flex-col">
+      <HeadHtml>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.5.0/styles/default.min.css"
+        />
+      </HeadHtml>
       <Header />
       <div className="flex-grow flex overflow-auto">
         <div className="w-60 bg-gray-800">
