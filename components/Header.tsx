b2b0f6e354653dde1c7f3218c16a606c3ea3d65c
Alexandre Giordanelli <alexandre.giordanelli@gmail.com>
2021-03-13 15:14:33 -0300

    change

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,9 +4,10 @@ import { Logo, LogoTextual } from './Logo'
 import { useRouter } from 'next/router'
 import { useAuth } from 'lib/auth'
 import firebase from 'lib/firebase-client'
-import { ChevronRightIcon } from '@primer/octicons-react'
+import { ChevronDownIcon, ChevronRightIcon } from '@primer/octicons-react'
 import NoSSR from 'react-no-ssr'
 import { Login } from './Login'
+import { getURLMedia } from 'lib/utils'
 
 export const Header: React.FC = () => {
   const { user } = useAuth()
@@ -21,13 +22,13 @@ export const Header: React.FC = () => {
     <>
       <div
         className={`bg-gray-800 shadow-lg top-0 sticky h-16 items-center justify-between flex p-1 z-10 ${
-          offsetPaddingLeft ? 'pl-8 lg:pl-2' : 'pl-2'
+          offsetPaddingLeft ? 'pl-8 lg:px-4' : 'px-4'
         }`}
       >
         <div className="items-center flex">
           <Link href="/">
             <a className="items-center flex">
-              <Logo size={32} color="#fff" className="mx-2" />
+              <Logo size={32} color="#fff" className="mr-2" />
               <LogoTextual size={32} color="#fff" className="hidden sm:block" />
             </a>
           </Link>
@@ -78,7 +79,7 @@ export const Header: React.FC = () => {
         <NoSSR>
           {!user && <Login className="mr-2" />}
           {user && (
-            <div className="flex">
+            <div className="flex items-center">
               {!router.pathname.startsWith('/admin') && (
                 <button
                   className="bg-gray-800 text-white text-sm rounded-md px-4 py-2 mr-2 border-gray-700 border hidden md:block"
@@ -87,16 +88,90 @@ export const Header: React.FC = () => {
                   Admin
                 </button>
               )}
-              <button
-                className="text-white px-4 py-2 mr-2 text-sm "
-                onClick={() => firebase.auth().signOut()}
-              >
-                Sign out
-              </button>
+              <DropDownMenu />
             </div>
           )}
         </NoSSR>
       </div>
     </>
+  )
+}
+
+const DropDownMenu: React.FC = () => {
+  const { customer } = useAuth()
+
+  return (
+    <div className="relative inline-block text-left">
+      <label htmlFor="dropDownMenu" className="flex items-center text-gray-700 cursor-pointer">
+        <img className="h-8 w-8 mr-1 rounded-full" src={getURLMedia(customer.media)} alt="avatar" />
+        <ChevronDownIcon />
+      </label>
+      <input
+        id="dropDownMenu"
+        className="toggleVisibilityUL"
+        type="checkbox"
+        // onChange={(x) => setToggleMenu(x.target.checked)}
+        // checked={toggleMenu}
+      />
+
+      <ul className="hidden origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none">
+        <li className="py-1">
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Edit
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Duplicate
+          </a>
+        </li>
+        <li className="py-1">
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Archive
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Move
+          </a>
+        </li>
+        <li className="py-1">
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Share
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            role="menuitem"
+          >
+            Add to favorites
+          </a>
+        </li>
+        <li className="py-1 flex">
+          <button
+            className="flex-1 text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+            onClick={() => firebase.auth().signOut()}
+          >
+            Sign out
+          </button>
+        </li>
+      </ul>
+    </div>
   )
 }
